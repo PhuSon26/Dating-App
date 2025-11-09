103899a0b9769deff4db2b668100414071a5fb17
PhuSon26 <leson2185@gmail.com>
2025-11-09 20:52:46 +0700

    them 3 form gioi thieu ung dung, doi email mat khau, danh sach chan

---
+++ Main Interface/User Controls/CaiDat_UserControls/DoiEmailMatKhau.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main_Interface.User_Controls.CaiDat_UserControls
+{
+    public partial class DoiEmailMatKhau : Form
+    {
+        public DoiEmailMatKhau()
+        {
+            InitializeComponent();
+        }
+    }
+}
